aef636be04000377b59ab5c555cf7e9194162faa
yuuki <you@example.com>
2020-02-23 21:48:52 +0900

    added

---
+++ ippon/abc156/b.py
@@ -0,0 +1,12 @@
+
+
+
+n,k = map(int,input().split())
+def Base_10_to_n(X, n):
+    X_dumy = X
+    out = ''
+    while X_dumy>0:
+        out = str(X_dumy%n)+out
+        X_dumy = int(X_dumy/n)
+    return out
+print(len(Base_10_to_n(n,k)))
