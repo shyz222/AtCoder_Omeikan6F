dd305de4155f13933afa424b65150e6f2a224a70
hayapo <keep.forward1001@gmail.com>
2020-05-18 02:51:24 +0900

    :poop:fix bfs.py

--- Blue/python/library/bfs.py
+++ Blue/python/library/bfs.py
@@ -8,9 +8,15 @@ dist = [inf for i in range(N)]
 visited = [0 for i in range(N)]
 
 for _ in range(M):
+    #辺をpathに追加
     a,b = map(int,input().split())
+    #インデックスのため-1
     a = a - 1
     b = b - 1
+    """
+    頂点aからb,bからaへのパスを追加
+    フォーマットは[[b],[a,c,d]]のようになる
+    """
     path[a].append(b)
     path[b].append(a)
 
