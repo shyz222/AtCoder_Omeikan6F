d6649deb815672ec80caf970c341a0a05df51dda
hayapo <keep.forward1001@gmail.com>
2021-06-23 20:01:58 +0900

    :art: Finished ABC103 Problem A,B,C

---
+++ Blue/C++/Atcoder_Problems/ABC103/a.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define REP(i, n) for (int i = 0; i < n; i++)
+#define REPR(i, n) for(int i = n; i >= 0; i--)
+#define FOR(i, m, n) for(int i = m; i < n; i++)
+#define endl    '\n'
+#define INF 2e9
+#define int ll
+#define vi          vector<int>
+#define de          deque<int>
+#define SZ(x) ((int)(x).size())
+#define all(x) (x).begin(),(x).end()//全範囲指定を簡略化
+typedef long long ll;
+using namespace std;
+//配列の要素数は　sizeof(ar)/sizeof(ar[0]) で求める
+signed main(){
+    cin.tie(0);
+    cout.tie(0);
+    ios::sync_with_stdio(false);
+    vi vec(3);
+    REP(i,3){
+        cin >> vec[i];
+    }
+    sort(vec.begin(), vec.end());
+    int ans = 0;
+    REP(i,2){
+            ans += max(vec[i]-vec[i+1], -vec[i] + vec[i+1]);
+    }
+    cout << ans << endl;
+}
