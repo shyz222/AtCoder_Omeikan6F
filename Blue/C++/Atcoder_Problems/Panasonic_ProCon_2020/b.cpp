7a23203927d69fecb0fe37d3bb93679b4c8fdfef
hayapo <keep.forward1001@gmail.com>
2021-06-27 18:57:43 +0900

    :art: Finished Panasonic ProCon 2020

---
+++ Blue/C++/Atcoder_Problems/Panasonic_ProCon_2020/b.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define REP(i, a, n) for (int i = a; i < n; i++)
+#define REPR(i, n) for(int i = n; i >= 0; i--)
+#define FOR(i, m, n) for(int i = m; i < n; i++)
+#define endl    '\n'
+#define INF 2e9
+#define int ll
+#define vi          vector<int>
+#define de          deque<int>
+#define SZ(x) ((int)(x).size())
+#define all(x) (x).begin(),(x).end()//全範囲指定を簡略化
+typedef long long ll;
+using namespace std;
+//配列の要素数は　sizeof(ar)/sizeof(ar[0]) で求める
+signed main(){
+    cin.tie(0);
+    cout.tie(0);
+    ios::sync_with_stdio(false);
+    int H,W;
+    cin>>H>>W;
+    int ans = H*W/2;
+    if(H==1 or W==1){
+        cout << 1 << endl;
+    }
+    else{
+        if((H*W)%2 == 1){
+            ans++;
+        }
+        cout << ans << endl;
+    }
+}
