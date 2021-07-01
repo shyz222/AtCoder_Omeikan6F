621f00ee84824a882e711aa7382d821f3fe74c6a
hayapo <keep.forward1001@gmail.com>
2021-07-02 00:12:39 +0900

    :zap: Finished ABC138/C

---
+++ Blue/C++/Atcoder_Problems/ABC138/c.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define REP(i, a, n) for (int i = a; i < n; i++)
+#define REPR(i, n) for(int i = n; i >= 0; i--)
+#define FOR(i, m, n) for(int i = m; i < n; i++)
+#define endl    '\n'
+#define INF 2e9
+#define int ll
+#define vi          vector<int>
+#define de          deque<int>
+#define SZ(x) ((int)(x).size())
+#define all(x) (x).begin(),(x).end()//全範囲指定を簡略化
+typedef long long ll;
+using namespace std;
+//配列の要素数は　sizeof(ar)/sizeof(ar[0]) で求める
+signed main(){
+    cin.tie(0);
+    cout.tie(0);
+    ios::sync_with_stdio(false);
+    int N;
+    cin>>N;
+    vi v(N);
+    REP(i,0,N){
+        cin >> v[i];
+    }
+    sort(v.begin(),v.end());
+    double ans = v[0];
+    REP(i,1,N){
+        ans = (ans + v[i])/2;
+    }
+    cout << ans << endl;
+}
