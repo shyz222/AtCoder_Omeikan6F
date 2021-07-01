ce77331c7ca0eae24ae0abadaaa8644aa18d96fd
hayapo <keep.forward1001@gmail.com>
2021-07-02 00:09:33 +0900

    :zap: Finished ABC116
    重複判定するときはsetを使う

---
+++ Blue/C++/Atcoder_Problems/ABC116/b.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+#define REP(i, a, n) for (int i = a; i < n; i++)
+#define REPR(i, n) for(int i = n; i >= 0; i--)
+#define FOR(i, m, n) for(int i = m; i < n; i++)
+#define endl    '\n'
+#define INF 2e9
+#define int ll
+#define vi          vector<int>
+#define de          deque<int>
+#define SZ(x) ((int)(x).size())
+#define all(x) (x).begin(),(x).end()//全範囲指定を簡略化
+typedef long long ll;
+using namespace std;
+//配列の要素数は　sizeof(ar)/sizeof(ar[0]) で求める
+signed main(){
+    cin.tie(0);
+    cout.tie(0);
+    ios::sync_with_stdio(false);
+    int S;
+    cin>>S;
+
+    set<int> done;
+    done.insert(S);
+    int tmp = S;
+    REP(i,2,1000000){
+        int nxt;
+        if(tmp%2==0){
+            nxt = tmp/2;
+        }
+        else{
+            nxt = 3*tmp+1;
+        }
+
+        if(done.count(nxt)){
+            cout << i << endl;
+            return 0;
+        }
+        done.insert(nxt);
+        tmp = nxt;
+    }
+}
