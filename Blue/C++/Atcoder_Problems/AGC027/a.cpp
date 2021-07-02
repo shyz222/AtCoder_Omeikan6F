af8c0071419e2c2a6aac61c5a81fd8b06904b6f2
hayapo <keep.forward1001@gmail.com>
2021-07-03 03:21:41 +0900

    :zap: Finished ABC027/C

---
+++ Blue/C++/Atcoder_Problems/AGC027/a.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#define REP(i, a, n) for (int i = a; i < n; ++i)
+#define REPR(i, n) for(int i = n; i >= 0; i--)
+#define FOR(i, m, n) for(int i = m; i < n; i++)
+#define endl    '\n'
+#define INF 2e9
+#define int ll
+#define vi          vector<int>
+#define de          deque<int>
+#define SZ(x) ((int)(x).size())
+#define all(x) (x).begin(),(x).end()//全範囲指定を簡略化
+typedef long long ll;
+using namespace std;
+//配列の要素数は　sizeof(ar)/sizeof(ar[0]) で求める
+signed main(){
+    cin.tie(0);
+    cout.tie(0);
+    ios::sync_with_stdio(false);
+    int N,x;
+    cin>>N>>x;
+    vi a(N);
+    REP(i,0,N){
+        cin >> a[i];
+    }
+    int ans = 0;
+    sort(all(a));
+    REP(i,0,N){
+        if(x>=a[i]){
+            ++ans;
+            x -= a[i];
+        }
+        else break;
+    }
+    if(ans == N && x>0){
+        ans -= 1;
+    }
+    cout << ans << endl;
+}
